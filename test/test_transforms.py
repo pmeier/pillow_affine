26aa801e692c815c311575f795f478808bdd1113
Philip Meier <philip.meier@th-owl.de>
2020-02-21 16:17:01 +0100

    Test transforms __repr__() (#13)

--- test/test_transforms.py
+++ test/test_transforms.py
@@ -1,6 +1,7 @@
 from typing import Tuple
 from os import path
 import unittest
+import re
 from pyimagetest import ImageTestcase
 from pillow_affine import transforms
 
@@ -25,13 +26,48 @@ def convert_translation(
     return translate
 
 
+def has_valid_single_transform_repr(transform, special_chars=""):
+    name = transform.__class__.__name__
+    valid_extra_repr_chars = r"\w\s.,=()" + special_chars
+    valid_pattern = name + f"[(][{valid_extra_repr_chars}]*[)]"
+    return re.fullmatch(valid_pattern, repr(transform)) is not None
+
+
 class Tester(ImageTestcase, unittest.TestCase):
     @property
     def default_test_image_file(self) -> str:
         here = path.abspath(path.dirname(__file__))
         return path.join(here, "..", "images", "raw.png")
 
-    def test_rotate(self):
+    def test_AffineTransform(self):
+        with self.assertRaises(TypeError):
+            transforms.AffineTransform()
+
+    def test_ElementaryTransform(self):
+        with self.assertRaises(TypeError):
+            transforms.ElementaryTransform()
+
+    def test_Shear(self):
+        angle = 30.0
+
+        transform = transforms.Shear(angle)
+        self.assertTrue(has_valid_single_transform_repr(transform, special_chars="°"))
+
+    def test_Shear_clockwise(self):
+        angle = 30.0
+        clockwise = True
+
+        transform = transforms.Shear(angle, clockwise=clockwise)
+        self.assertTrue(has_valid_single_transform_repr(transform, special_chars="°"))
+
+    def test_Shear_off_image_center(self):
+        angle = 30.0
+        center = (0.0, 0.0)
+
+        transform = transforms.Shear(angle, center=center)
+        self.assertTrue(has_valid_single_transform_repr(transform, special_chars="°"))
+
+    def test_Rotate(self):
         image = self.load_image("PIL")
         angle = 30.0
 
@@ -42,8 +78,9 @@ class Tester(ImageTestcase, unittest.TestCase):
         desired = image.rotate(convert_angle(angle))
 
         self.assertImagesAlmostEqual(actual, desired)
+        self.assertTrue(has_valid_single_transform_repr(transform, special_chars="°"))
 
-    def test_rotate_clockwise(self):
+    def test_Rotate_clockwise(self):
         image = self.load_image("PIL")
         angle = 30.0
         clockwise = True
@@ -55,8 +92,9 @@ class Tester(ImageTestcase, unittest.TestCase):
         desired = image.rotate(convert_angle(angle, clockwise=clockwise))
 
         self.assertImagesAlmostEqual(actual, desired)
+        self.assertTrue(has_valid_single_transform_repr(transform, special_chars="°"))
 
-    def test_rotate_off_image_center(self):
+    def test_Rotate_off_image_center(self):
         image = self.load_image("PIL")
         angle = 30.0
         image_center = transforms.calculate_image_center(image.size)
@@ -69,8 +107,28 @@ class Tester(ImageTestcase, unittest.TestCase):
         desired = image.rotate(angle, center=convert_center(center, image.size))
 
         self.assertImagesAlmostEqual(actual, desired)
+        self.assertTrue(has_valid_single_transform_repr(transform, special_chars="°"))
 
-    def test_translate(self):
+    def test_Scale(self):
+        factor = 0.5
+
+        transform = transforms.Scale(factor)
+        self.assertTrue(has_valid_single_transform_repr(transform))
+
+    def test_Scale_async(self):
+        factor = (0.5, 2.0)
+
+        transform = transforms.Scale(factor)
+        self.assertTrue(has_valid_single_transform_repr(transform))
+
+    def test_Scale_off_image_center(self):
+        factor = 0.5
+        center = (0.0, 0.0)
+
+        transform = transforms.Scale(factor, center=center)
+        self.assertTrue(has_valid_single_transform_repr(transform))
+
+    def test_Translate(self):
         image = self.load_image("PIL")
         translation = (100.0, 50.0)
 
@@ -81,8 +139,9 @@ class Tester(ImageTestcase, unittest.TestCase):
         desired = image.rotate(0.0, translate=convert_translation(translation))
 
         self.assertImagesAlmostEqual(actual, desired)
+        self.assertTrue(has_valid_single_transform_repr(transform))
 
-    def test_translate_inverse(self):
+    def test_Translate_inverse(self):
         image = self.load_image("PIL")
         translation = (100.0, 50.0)
         inverse = True
@@ -96,18 +155,19 @@ class Tester(ImageTestcase, unittest.TestCase):
         )
 
         self.assertImagesAlmostEqual(actual, desired)
+        self.assertTrue(has_valid_single_transform_repr(transform))
 
-    def test_empty_composed(self):
+    def test_empty_ComposedTransform(self):
         with self.assertRaises(RuntimeError):
             transforms.ComposedTransform()
 
-    def test_composed(self):
+    def test_ComposedTransform(self):
         image = self.load_image("PIL")
         angle = 30.0
         translation = (100.0, 50.0)
 
         transform = transforms.ComposedTransform(
-            transforms.Rotate(angle), transforms.Translate(translation),
+            transforms.Rotate(angle), transforms.Translate(translation)
         )
         transform_params = transform.extract_transform_params(image.size)
         actual = image.transform(*transform_params)
